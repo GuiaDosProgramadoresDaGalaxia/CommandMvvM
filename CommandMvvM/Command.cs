ee21dd3bb7fc5e481b6b281d12d6b83a916b8591
Filipe Santiago <porto.santiago@hotmail.com>
2016-12-20 22:23:00 -0300

    Adicionar arquivos de projeto.

---
+++ CommandMvvM/Command.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CommandMvvM
+{
+    public class Command : ICommand
+    {
+        public Action Action { get; set; }
+        public string DisplayName { get; set; }
+
+        private bool _isEnabled = true;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public Command(Action action)
+        {
+            Action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+
+        public void Execute(object parameter)
+        {
+            Action?.Invoke();
+        }
+    }
+
+    public class Command<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public Action<T> Action { get; set; }
+        public string DisplayName { get; set; }
+        private bool _isEnabled = true;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        public Command(Action<T> action)
+        {
+            Action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+        public void Execute(object parameter)
+        {
+            if (Action != null && parameter is T)
+                Action((T)parameter);
+        }
+    }
+}
---
+++ CommandMvvM/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace CommandMvvM
+{
+    public class ViewModel
+    {
+        public Command MeuCommand { get; set; }
+        public Command<string> MeuCommand2 { get; set; }
+
+        public ViewModel()
+        {
+            MeuCommand = new Command(Acao);
+            MeuCommand2 = new Command<string>(Acao2);
+        }
+
+        protected void Acao2(string obj)
+        {
+            MessageBox.Show(obj);
+        }
+
+        protected void Acao()
+        {
+            MessageBox.Show("Meu primeiro Command!");
+        }
+    }
+}
